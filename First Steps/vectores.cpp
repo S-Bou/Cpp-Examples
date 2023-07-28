910eed2cd92ec80d200c80584948ad22d5473060
S-Bou <bouyo30@gmail.com>
2023-07-28 22:47:16 +0200

    202307282247

--- First Steps/vectores.cpp
+++ First Steps/vectores.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
     vector <int> records;
     vector <int> records1 (5);
@@ -22,9 +22,47 @@ int main(int argc, char const *argv[])
 
     int j = 0;
     while(j < records5.size()){
-        cout << records5[j] << endl;
+        cout << records5[j] << " ";
         j++;
     }
+    cout << endl;
+
+    // Vectores con comprobación de límites.
+    records3.at(3) = 105;
+
+    cout << records3.at(3) << endl;
+
+    records3.push_back(106);
+
+    cout << records3.at(5) << endl;
+
+    for (int i = 0; i < records3.size(); i++){
+        cout << records3[i] << endl;
+    }
+
+    int elementos = 0;
+    int nuevodato;
+
+    cout << "Numero de elementos?" << endl;
+
+    cin >> elementos;
+
+    vector <int> records6 (elementos);
+
+    for (int i = 0; i < elementos; i++){
+        cout << "Introduce el elemento n: " << i+1 << endl;
+        //cin >> records6[i]; 
+        cin >> nuevodato;
+        records6.push_back(nuevodato); 
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < records6.size(); i++){
+        cout << records6.at(i) << endl;
+    }
+
+
 
     //getch ();
 }
